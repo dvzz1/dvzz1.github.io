c5190fe375dae5aba53b851e1d8f83c35438ee7b
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 13:59:16 -0600

    fixes

--- script.js
+++ script.js
@@ -1,58 +1,48 @@
-// Load the Google API Client Library
-gapi.load('client', start);
+// Initialize Google Identity Services (GIS)
+google.accounts.id.initialize({ client_id: 'YOUR_CLIENT_ID' }); // Replace 'YOUR_CLIENT_ID' with your actual client ID
 
-function start() {
-  // Initialize the Google API Client with your API key
-  gapi.client.init({
-    apiKey: 'AIzaSyBM-Yl64gfsH26SE9bmXo8V45RxPFVjDEM',
-    clientId: '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com',
-    scope: 'https://www.googleapis.com/auth/documents',
-  }).then(function () {
-    // Listen for the form submission event
-    const form = document.getElementById('typingForm');
-    const responseMessage = document.getElementById('responseMessage');
+// Listen for the form submission event
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('typingForm');
+  const responseMessage = document.getElementById('responseMessage');
 
-    form.addEventListener('submit', async function (e) {
-      e.preventDefault();
+  form.addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-      const textToType = document.getElementById('textToType').value;
+    const textToType = document.getElementById('textToType').value;
 
-      if (textToType) {
-        // Authorize the user
-        const auth = gapi.auth2.getAuthInstance();
+    if (textToType) {
+      try {
+        const auth = google.accounts.id.getGsiAuth({ client_id: 'YOUR_CLIENT_ID' }); // Replace 'YOUR_CLIENT_ID' with your actual client ID
+        const signInResponse = await auth.signIn();
 
-        // Trigger OAuth authentication if the user is not signed in
-        if (!auth.isSignedIn.get()) {
-          try {
-            await auth.signIn();
-          } catch (error) {
-            console.error('OAuth authentication error:', error);
-            return;
+        if (signInResponse['status'] === 'OK') {
+          // User is signed in
+          const response = await fetch('https://script.google.com/macros/s/AKfycbysWGBdwKPRd5WWtSm7_l6DToC3EN6jKcwIsPm5fiC0yMhFqyQHc4pWNRkrBotFcRJS3A/exec', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Authorization: 'Bearer ' + auth.currentUser.get().getAuthResponse().access_token,
+            },
+            body: `textToType=${encodeURIComponent(textToType)}`,
+          });
+
+          if (response.ok) {
+            const result = await response.text();
+            responseMessage.textContent = result;
+          } else {
+            responseMessage.textContent = 'Error: Unable to connect to the server.';
           }
-        }
-
-        // Make a request to your Google Apps Script Web App
-        const response = await fetch('https://script.google.com/macros/s/AKfycbysWGBdwKPRd5WWtSm7_l6DToC3EN6jKcwIsPm5fiC0yMhFqyQHc4pWNRkrBotFcRJS3A/exec', {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Authorization: 'Bearer ' + auth.currentUser.get().getAuthResponse().access_token,
-          },
-          body: `textToType=${encodeURIComponent(textToType)}`,
-        });
-
-        if (response.ok) {
-          const result = await response.text();
-          responseMessage.textContent = result;
         } else {
-          responseMessage.textContent = 'Error: Unable to connect to the server.';
+          // Handle sign-in error
+          console.error('Sign-in error:', signInResponse);
         }
-      } else {
-        responseMessage.textContent = 'Error: Text to type is missing.';
+      } catch (error) {
+        console.error('Sign-in error:', error);
       }
-    });
-  }).catch(function (error) {
-    console.log(error);
+    } else {
+      responseMessage.textContent = 'Error: Text to type is missing.';
+    }
   });
-}
+});
