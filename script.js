92790dc7ed0fe93a83704bd2d04e1c4204320031
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 14:07:05 -0600

    ddd

--- script.js
+++ script.js
@@ -1,49 +1,116 @@
-// Listen for the form submission event
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('typingForm');
-  const responseMessage = document.getElementById('responseMessage');
+// Load the Google API Client Library
+gapi.load('client', start);
 
-  form.addEventListener('submit', async function (e) {
-    e.preventDefault();
+function start() {
+  // Initialize the Google API Client with your API key
+  gapi.client.init({
+    apiKey: 'AIzaSyBM-Yl64gfsH26SE9bmXo8V45RxPFVjDEM',
+    clientId: '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/documents',
+  }).then(function () {
+    // Listen for the form submission event
+    const form = document.getElementById('typingForm');
+    const responseMessage = document.getElementById('responseMessage');
 
-    const textToType = document.getElementById('textToType').value;
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
 
-    if (textToType) {
-      try {
-        const auth = google.accounts.id.getAuthCode({
-          client_id: '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com', // Replace with your Google Cloud Console project's client ID
-          prompt_parent_id: 'content'
+      const textToType = document.getElementById('textToType').value;
+
+      if (textToType) {
+        // Authorize the user
+        const auth = gapi.auth2.getAuthInstance();
+
+        // Trigger OAuth authentication if the user is not signed in
+        if (!auth.isSignedIn.get()) {
+          try {
+            await auth.signIn();
+          } catch (error) {
+            console.error('OAuth authentication error:', error);
+            return;
+          }
+        }
+
+        // Make a request to your Google Apps Script Web App
+        const response = await fetch('https://script.google.com/macros/s/AKfycbysWGBdwKPRd5WWtSm7_l6DToC3EN6jKcwIsPm5fiC0yMhFqyQHc4pWNRkrBotFcRJS3A/exec', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Bearer ' + auth.currentUser.get().getAuthResponse().access_token,
+          },
+          body: `textToType=${encodeURIComponent(textToType)}`,
         });
 
-        const signInResponse = await auth.signIn();
-
-        if (signInResponse['status'] === 'OK') {
-          // User is signed in
-          const response = await fetch('https://script.google.com/macros/s/AKfycbysWGBdwKPRd5WWtSm7_l6DToC3EN6jKcwIsPm5fiC0yMhFqyQHc4pWNRkrBotFcRJS3A/exec', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              Authorization: 'Bearer ' + signInResponse['auth_code'],
-            },
-            body: `textToType=${encodeURIComponent(textToType)}`,
-          });
-
-          if (response.ok) {
-            const result = await response.text();
-            responseMessage.textContent = result;
-          } else {
-            responseMessage.textContent = 'Error: Unable to connect to the server.';
-          }
+        if (response.ok) {
+          const result = await response.text();
+          responseMessage.textContent = result;
         } else {
-          // Handle sign-in error
-          console.error('Sign-in error:', signInResponse);
+          responseMessage.textContent = 'Error: Unable to connect to the server.';
         }
-      } catch (error) {
-        console.error('Sign-in error:', error);
+      } else {
+        responseMessage.textContent = 'Error: Text to type is missing.';
       }
-    } else {
-      responseMessage.textContent = 'Error: Text to type is missing.';
-    }
+    });
+  }).catch(function (error) {
+    console.log(error);
   });
-});
+}
+// Load the Google API Client Library
+gapi.load('client', start);
+
+function start() {
+  // Initialize the Google API Client with your API key
+  gapi.client.init({
+    apiKey: 'AIzaSyBM-Yl64gfsH26SE9bmXo8V45RxPFVjDEM',
+    clientId: '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/documents',
+  }).then(function () {
+    // Listen for the form submission event
+    const form = document.getElementById('typingForm');
+    const responseMessage = document.getElementById('responseMessage');
+
+    form.addEventListener('submit', async function (e) {
+      e.preventDefault();
+
+      const textToType = document.getElementById('textToType').value;
+
+      if (textToType) {
+        // Authorize the user
+        const auth = gapi.auth2.getAuthInstance();
+
+        // Trigger OAuth authentication if the user is not signed in
+        if (!auth.isSignedIn.get()) {
+          try {
+            await auth.signIn();
+          } catch (error) {
+            console.error('OAuth authentication error:', error);
+            return;
+          }
+        }
+
+        // Make a request to your Google Apps Script Web App
+        const response = await fetch('https://script.google.com/macros/s/AKfycbysWGBdwKPRd5WWtSm7_l6DToC3EN6jKcwIsPm5fiC0yMhFqyQHc4pWNRkrBotFcRJS3A/exec', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Bearer ' + auth.currentUser.get().getAuthResponse().access_token,
+          },
+          body: `textToType=${encodeURIComponent(textToType)}`,
+        });
+
+        if (response.ok) {
+          const result = await response.text();
+          responseMessage.textContent = result;
+        } else {
+          responseMessage.textContent = 'Error: Unable to connect to the server.';
+        }
+      } else {
+        responseMessage.textContent = 'Error: Text to type is missing.';
+      }
+    });
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
