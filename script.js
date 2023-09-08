5dd5c8375f104a93a64ab3690e8c461a18ff0a04
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 16:32:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
   const responseMessage = document.getElementById('responseMessage');
   const googleAppsScriptFrame = document.getElementById('googleAppsScriptFrame');
 
-  form.addEventListener('submit', function (e) {
+  form.addEventListener('submit', async function (e) {
     e.preventDefault();
 
     const textToType = document.getElementById('textToType').value;
 
     if (textToType) {
-      // Send the text to your Google Apps Script web app via the iframe
-      googleAppsScriptFrame.contentWindow.postMessage(textToType, 'https://script.google.com');
+      try {
+        // Send the text to your Google Apps Script web app via the iframe
+        googleAppsScriptFrame.contentWindow.postMessage(textToType, 'https://script.google.com/macros/s/AKfycbz0ahKj7DC16bvcVN1Wd2UUwRZh0hIbKUvRY-j45dULb5y78-0Uzeouwshak-1uCTc_Vg/exec');
+        responseMessage.textContent = 'Sending text to Google Apps Script...';
+
+        // Add a timeout to wait for a response (adjust as needed)
+        const timeoutDuration = 10000; // 10 seconds
+        const timeout = setTimeout(() => {
+          responseMessage.textContent = 'Timeout: No response from Google Apps Script.';
+        }, timeoutDuration);
+
+        // Listen for messages from the embedded iframe
+        window.addEventListener('message', function (event) {
+          if (event.origin === 'https://script.google.com/macros/s/AKfycbz0ahKj7DC16bvcVN1Wd2UUwRZh0hIbKUvRY-j45dULb5y78-0Uzeouwshak-1uCTc_Vg/exec') {
+            clearTimeout(timeout); // Clear the timeout
+            const result = event.data;
+            responseMessage.textContent = result;
+          }
+        });
+      } catch (error) {
+        responseMessage.textContent = 'Error: ' + error.message;
+      }
     } else {
       responseMessage.textContent = 'Error: Text to type is missing.';
-    }
-  });
-
-  // Listen for messages from the embedded iframe
-  window.addEventListener('message', function (event) {
-    if (event.origin === 'https://script.google.com/macros/s/AKfycbz0ahKj7DC16bvcVN1Wd2UUwRZh0hIbKUvRY-j45dULb5y78-0Uzeouwshak-1uCTc_Vg/exec') {
-      // Handle responses from the iframe (e.g., success or error messages)
-      const result = event.data;
-      responseMessage.textContent = result;
     }
   });
 });
