e7c650bc1e23da340f75b6954e28e490fd4a205e
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 16:20:53 -0600

    bb

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('typingForm');
   const responseMessage = document.getElementById('responseMessage');
-  const startTypingButton = document.getElementById('startTypingButton');
   const googleAppsScriptFrame = document.getElementById('googleAppsScriptFrame');
 
   form.addEventListener('submit', function (e) {
@@ -10,9 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const textToType = document.getElementById('textToType').value;
 
     if (textToType) {
-      console.log('Sending text to iframe:', textToType);
       // Send the text to your Google Apps Script web app via the iframe
-      googleAppsScriptFrame.contentWindow.postMessage(textToType, '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com');
+      googleAppsScriptFrame.contentWindow.postMessage(textToType, 'https://script.google.com');
     } else {
       responseMessage.textContent = 'Error: Text to type is missing.';
     }
@@ -20,10 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Listen for messages from the embedded iframe
   window.addEventListener('message', function (event) {
-    if (event.origin === '916884835094-nu61ir6f1tbkf0nu2363t01r8g4b3fot.apps.googleusercontent.com') {
+    if (event.origin === 'https://script.google.com/macros/s/AKfycbz0ahKj7DC16bvcVN1Wd2UUwRZh0hIbKUvRY-j45dULb5y78-0Uzeouwshak-1uCTc_Vg/exec') {
       // Handle responses from the iframe (e.g., success or error messages)
       const result = event.data;
-      console.log('Received message from iframe:', result);
       responseMessage.textContent = result;
     }
   });
