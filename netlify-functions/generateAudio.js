2f69d369cfad90e5c30acf0d40192b5d5b8df8a5
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 22:12:23 -0600

    Update and rename convertTextToAudio.js to generateAudio.js

--- netlify-functions/generateAudio.js
+++ netlify-functions/generateAudio.js
@@ -1,10 +1,10 @@
-const { textToSpeech } = require('your-text-to-speech-library');
+const { textToSpeech } = require('your-text-to-speech-library'); // Replace with the actual text-to-speech library you plan to use
 
 exports.handler = async function (event, context) {
   try {
     const { text } = JSON.parse(event.body);
 
-    // Generate audio from text using your chosen text-to-speech library
+    // Generate audio from text using your text-to-speech library
     const audioFile = await textToSpeech(text);
 
     return {
