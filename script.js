b1fe5330cf1926de053db6c4f4527824c3d439a7
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 21:01:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,34 +8,53 @@ pdfFileInput.addEventListener('change', () => {
         // Display the PDF in the iframe (You may need a PDF rendering library)
         pdfViewer.src = URL.createObjectURL(file);
 
-        // Extract text from PDF and read it out loud
-        readPDFText(file);
+        // Extract text from PDF and convert it to audio
+        extractTextAndConvertToAudio(file);
     }
 });
 
-function readPDFText(pdfFile) {
-    const reader = new FileReader();
+function extractTextAndConvertToAudio(pdfFile) {
+    // Initialize pdf.js
+    pdfjsLib.getDocument(pdfFile).promise.then(function(pdfDoc) {
+        let text = '';
 
-    reader.onload = function () {
-        const text = reader.result;
+        // Iterate through each page and extract text
+        for (let pageNum = 1; pageNum <= pdfDoc.numPages; pageNum++) {
+            pdfDoc.getPage(pageNum).then(function(page) {
+                return page.getTextContent();
+            }).then(function(pageText) {
+                // Combine text from all pages
+                for (const item of pageText.items) {
+                    text += item.str + ' ';
+                }
 
-        // Create an utterance for text-to-speech
-        const speechUtterance = new SpeechSynthesisUtterance(text);
+                // If this is the last page, convert text to audio
+                if (pageNum === pdfDoc.numPages) {
+                    convertTextToAudio(text);
+                }
+            });
+        }
+    }).catch(function(error) {
+        console.error('Error loading PDF: ' + error.message);
+    });
+}
 
-        // Set the voice and other options (adjust as needed)
-        speechUtterance.lang = 'en-US';
-        speechUtterance.rate = 1.0;
+function convertTextToAudio(text) {
+    // Create an utterance for text-to-speech
+    const speechUtterance = new SpeechSynthesisUtterance(text);
 
-        // Use the Web Speech API to speak the text
-        window.speechSynthesis.speak(speechUtterance);
+    // Set the voice and other options (adjust as needed)
+    speechUtterance.lang = 'en-US';
+    speechUtterance.rate = 1.0;
 
-        // Enable the audio player with synthesized speech
-        audioPlayer.src = URL.createObjectURL(new Blob([text], { type: 'text/plain' }));
-        audioPlayer.load();
+    // Use the Web Speech API to speak the text
+    window.speechSynthesis.speak(speechUtterance);
 
-        // Display a confirmation message
-        alert('PDF uploaded and audio loaded successfully!');
-    };
+    // Enable the audio player with synthesized speech
+    const audioBlob = new Blob([text], { type: 'text/plain' });
+    audioPlayer.src = URL.createObjectURL(audioBlob);
+    audioPlayer.load();
 
-    reader.readAsArrayBuffer(pdfFile);
+    // Display a confirmation message
+    alert('PDF uploaded and audio loaded successfully!');
 }
