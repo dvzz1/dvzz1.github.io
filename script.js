7a4ee40167914c2f3c50a5de2b01f2b0758db08b
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 20:51:57 -0600

    2

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-<script>
-    const pdfFileInput = document.getElementById('pdfFile');
-    const pdfViewer = document.getElementById('pdfViewer');
-    const audioPlayer = document.getElementById('audioPlayer');
+const pdfFileInput = document.getElementById('pdfFile');
+const pdfViewer = document.getElementById('pdfViewer');
+const audioPlayer = document.getElementById('audioPlayer');
 
-    pdfFileInput.addEventListener('change', () => {
-        const file = pdfFileInput.files[0];
-        if (file) {
-            // Display the PDF in the iframe (You may need a PDF rendering library)
-            pdfViewer.src = URL.createObjectURL(file);
+pdfFileInput.addEventListener('change', () => {
+    const file = pdfFileInput.files[0];
+    if (file) {
+        // Display the PDF in the iframe (You may need a PDF rendering library)
+        pdfViewer.src = URL.createObjectURL(file);
 
-            // Extract text from PDF and read it out loud using Web Speech API
-            readPDFText(file);
-        }
-    });
-
-    function readPDFText(pdfFile) {
-        const reader = new FileReader();
-
-        reader.onload = function () {
-            const text = reader.result;
-
-            // Create an utterance for text-to-speech
-            const speechUtterance = new SpeechSynthesisUtterance(text);
-
-            // Set the voice and other options (adjust as needed)
-            speechUtterance.lang = 'en-US';
-            speechUtterance.rate = 1.0;
-
-            // Use the Web Speech API to speak the text
-            window.speechSynthesis.speak(speechUtterance);
-
-            // Enable the audio player with synthesized speech
-            audioPlayer.src = URL.createObjectURL(new Blob([text], { type: 'text/plain' }));
-            audioPlayer.load();
-        };
-
-        reader.readAsText(pdfFile);
+        // Extract text from PDF and read it out loud using Web Speech API
+        readPDFText(file);
     }
-</script>
+});
+
+function readPDFText(pdfFile) {
+    const reader = new FileReader();
+
+    reader.onload = function () {
+        const text = reader.result;
+
+        // Create an utterance for text-to-speech
+        const speechUtterance = new SpeechSynthesisUtterance(text);
+
+        // Set the voice and other options (adjust as needed)
+        speechUtterance.lang = 'en-US';
+        speechUtterance.rate = 1.0;
+
+        // Use the Web Speech API to speak the text
+        window.speechSynthesis.speak(speechUtterance);
+
+        // Enable the audio player with synthesized speech
+        audioPlayer.src = URL.createObjectURL(new Blob([text], { type: 'text/plain' }));
+        audioPlayer.load();
+    };
+
+    reader.readAsText(pdfFile);
+}
