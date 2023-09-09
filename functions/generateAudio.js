3eaab437fea556efa2cf494b7ccc0f3209c7d077
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2023-09-08 23:06:10 -0600

    Update generateAudio.js

--- functions/generateAudio.js
+++ functions/generateAudio.js
@@ -4,14 +4,23 @@ exports.handler = async function (event, context) {
   try {
     const { text } = JSON.parse(event.body);
 
+    // Log the received text for debugging
+    console.log('Received text:', text);
+
     // Generate audio from text using your text-to-speech library
     const audioFile = await textToSpeech(text);
+
+    // Log the generated audio file path for debugging
+    console.log('Generated audio file:', audioFile);
 
     return {
       statusCode: 200,
       body: JSON.stringify({ audioFile }),
     };
   } catch (error) {
+    // Log any errors that occur during execution for debugging
+    console.error('Error:', error);
+
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
